5394e59e2c43dc2915dfae5814b0b73dc87532c6
omonomo <kasanel92@gmail.com>
2024-01-21 21:21:43 +0900

    [Update] スクリプトの簡素化と汎用化

--- fontsCollector/collectFonts.bat
+++ fontsCollector/collectFonts.bat
@@ -1,25 +1,12 @@
 @echo off
-:: フォントを集合させるプログラム
+:: サブフォルダにあるフォントを集合させるプログラム
 
-set FAMILY=Cyroit
-
-set SUFFIX0=BS
-set SUFFIX1=SP
-set SUFFIX2=FX
-set SUFFIX3=HB
-set SUFFIX4=DG
-::set SUFFIX5=DS
-:: set SUFFIX6=TM
-:: set SUFFIX7=TS
-
-for /f "usebackq delims== tokens=1,2" %%a in (`set SUFFIX`) do (
-  move .\%%b\%FAMILY%%%b-Bold.ttf .\
-  move .\%%b\%FAMILY%%%b-BoldOblique.ttf .\
-  move .\%%b\%FAMILY%%%b-Regular.ttf .\
-  move .\%%b\%FAMILY%%%b-Oblique.ttf .\
-
-  rd /q %%b
+cd /d %~dp0
+for /r %%a in ("*.ttf") do (
+  move "%%a" .\
 )
 
 :: del /q OFL.txt
 :: del /q README.md
+
+exit
--- fontsCollector/collectFonts.sh
+++ fontsCollector/collectFonts.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# フォントを集合させるプログラム
+# サブフォルダにあるフォントを集合させるプログラム
 
 sh_dir=$(cd $(dirname $0) && pwd)
-font_familyname="Cyroit"
-font_familyname_suffix=("BS" "SP" "FX" "HB" "DG")
-#font_familyname_suffix=("BS" "SP" "FX" "HB" "DG" "DS" "TM" "TS")
 
-for S in ${font_familyname_suffix[@]}; do
-  mv ${sh_dir}/${S}/${font_familyname}${S}-Bold.ttf ${sh_dir}/
-  mv ${sh_dir}/${S}/${font_familyname}${S}-BoldOblique.ttf ${sh_dir}/
-  mv ${sh_dir}/${S}/${font_familyname}${S}-Oblique.ttf ${sh_dir}/
-  mv ${sh_dir}/${S}/${font_familyname}${S}-Regular.ttf ${sh_dir}
-
-  rm -rf ${sh_dir}/${S}
+ls -F "${sh_dir}" | grep / | while read -r S; do
+  mv ${sh_dir}/${S}*.ttf ${sh_dir}/
 done
 
 #rm -f OFL.txt
