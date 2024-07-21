9d85b9d24125989a83c985afd5c1f404de59ab07
omonomo <kasanel92@gmail.com>
2024-07-21 21:32:42 +0900

    [Update] SS追加に対応

--- table_modificator.sh
+++ table_modificator.sh
@@ -382,10 +382,11 @@ if [ "${gsub_flag}" = "true" ]; then # caltListを作り直す場合は今ある
       <FeatureIndex index="19" value="23"/>\
       <FeatureIndex index="20" value="24"/>\
       <FeatureIndex index="21" value="25"/>\
+      <FeatureIndex index="22" value="26"/>\
       ,' "${P%%.ttf}.ttx" # index9を上書き、以降 index(12 + ss フィーチャの数)、value(index + 4) を追加
       if [ "${calt_ok_flag}" = "true" ]; then # calt対応であればさらに1つ index 追加
-        sed -i.bak -e 's,<FeatureIndex index="21" value=".."/>,<FeatureIndex index="21" value="25"/>\
-        <FeatureIndex index="22" value="26"/>\
+        sed -i.bak -e 's,<FeatureIndex index="22" value=".."/>,<FeatureIndex index="22" value="26"/>\
+        <FeatureIndex index="23" value="27"/>\
         ,' "${P%%.ttf}.ttx"
       fi
     else # ss非対応の場合
