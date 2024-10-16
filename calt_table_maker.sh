67afcaf7afa7534e6486577f44aae95e643f41db
omonomo <kasanel92@gmail.com>
2024-10-16 19:04:33 +0900

    [Update] font_generator の変更に対応

--- calt_table_maker.sh
+++ calt_table_maker.sh
@@ -18,10 +18,10 @@ exec 1> >(tee -a $LOG_OUT)
 exec 2> >(tee -a $LOG_ERR)
 #LOG
 
- #glyphNo="14392" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsなし、絵文字削減)
-glyphNo="14441" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsなし)
- #glyphNo="24482" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsあり、絵文字削減)
- #glyphNo="24532" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsあり)
+ #glyphNo="14394" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsなし、絵文字削減)
+glyphNo="14443" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsなし)
+ #glyphNo="24484" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsあり、絵文字削減)
+ #glyphNo="24534" # デフォルトのcalt用異体字の先頭glyphナンバー (Nerd Fontsあり)
 listNo="-1"
 optimizeListNo="4" # -o -O オプションが設定してある場合、指定の listNo 以下は最適化ルーチンを実行する
 caltListName="caltList" # caltテーブルリストの名称
